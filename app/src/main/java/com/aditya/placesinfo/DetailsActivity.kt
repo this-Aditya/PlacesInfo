25f7e22b824a6f9847ec20ee1fd10ee568ef3628
Aditya Mishra <adityamishra2217@gmail.com>
2023-07-23 16:18:21 +0530

    Added details activity

---
+++ app/src/main/java/com/aditya/placesinfo/DetailsActivity.kt
@@ -0,0 +1,53 @@
+package com.aditya.placesinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.aditya.placesinfo.databinding.ActivityDetailsBinding
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.ktx.api.net.awaitFetchPlace
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+private const val TAG = "DetailsActivity"
+
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
+    private lateinit var placesClient: PlacesClient
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        placesClient = Places.createClient(this)
+
+
+        binding.detailsButton.setOnClickListener {
+            val placeId = binding.detailsInput.text.toString()
+            val placeField = listOf(
+                    Place.Field.NAME,
+                    Place.Field.ID,
+                    Place.Field.LAT_LNG,
+                    Place.Field.ADDRESS,
+                    Place.Field.TYPES
+            )
+            lifecycleScope.launch {
+                try {
+                    val response = placesClient.awaitFetchPlace(placeId, placeField)
+                    binding.detailsResponseContent.text = response.toString()
+                    Log.i(TAG, "Response received: \n $response")
+                } catch (ex: Exception) {
+                    Log.w(TAG, "Exception while fetching the place: $ex")
+                    binding.detailsResponseContent.text = ex.message.toString()
+                }
+            }
+        }
+
+    }
+}
+
--- app/src/main/java/com/aditya/placesinfo/MainActivity.kt
+++ app/src/main/java/com/aditya/placesinfo/MainActivity.kt
@@ -2,11 +2,14 @@ package com.aditya.placesinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aditya.placesinfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
